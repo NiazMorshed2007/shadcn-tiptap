9171ee338245ca89c252691c6beb151ae03989ba
NiazMorshed2007 <niazmorshed2007@gmail.com>
2024-10-27 11:28:30 +0600

    feat: revamp docs with contentlayer

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -1,28 +1,22 @@
-"use client";
+import { DocsSidebarNav } from "@/components/sidebar-nav";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { docsConfig } from "@/config/docs";
 
-import {
-	NavigationDesktop,
-	NavigationMobile,
-} from "./_components/content-sidebar";
-import { TableOfContents } from "./_components/toc";
+interface DocsLayoutProps {
+	children: React.ReactNode;
+}
 
-const DocsLayout = ({ children }: React.PropsWithChildren) => {
+export default function DocsLayout({ children }: DocsLayoutProps) {
 	return (
-		<div className="max-w-7xl mx-auto flex flex-col md:flex-row md:space-x-12">
-			<aside>
-				<NavigationDesktop />
-				<NavigationMobile />
-			</aside>
-			<main className="prose px-6 md:px-0 prose-zinc min-w-0 max-w-full flex-1 pb-16 pt-8 dark:prose-invert prose-h1:scroll-m-20 prose-h1:text-2xl prose-h1:font-semibold prose-h2:scroll-m-20 prose-h2:text-xl prose-h2:font-medium prose-h3:scroll-m-20 prose-h3:text-base prose-h3:font-medium prose-h4:scroll-m-20 prose-h5:scroll-m-20 prose-h6:scroll-m-20 prose-strong:font-medium prose-table:block prose-table:overflow-y-auto lg:pt-12 xl:max-w-2xl">
-				{children}
-			</main>
-			<div className="w-3/12 hidden md:block">
-				<aside className="pl-6 py-12 sticky top-14">
-					<TableOfContents />
+		<div className="border-b">
+			<div className="container flex-1 items-start md:grid md:grid-cols-[220px_minmax(0,1fr)] md:gap-6 lg:grid-cols-[240px_minmax(0,1fr)] lg:gap-10">
+				<aside className="fixed top-14 z-30 -ml-2 hidden h-[calc(100vh-3.5rem)] w-full shrink-0 md:sticky md:block">
+					<ScrollArea className="h-full py-6 pr-6 lg:py-8">
+						<DocsSidebarNav config={docsConfig} />
+					</ScrollArea>
 				</aside>
+				{children}
 			</div>
 		</div>
 	);
-};
-
-export default DocsLayout;
+}
---
+++ src/config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+	name: "shadcn-tiptap",
+	url: "https://tiptap.niazmorshed.dev",
+	ogImage: "https://tiptap.niazmorshed.dev/og.jpg",
+	description:
+		"Set of custom extensions & toolbars for tiptap, made with shadcn/ui.",
+	links: {
+		twitter: "https://twitter.com/niazmorshed_",
+		github: "https://github.com/NiazMorshed2007/shadcn-tiptap",
+	},
+};
+
+export type SiteConfig = typeof siteConfig;
